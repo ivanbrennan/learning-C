11b1c277e23fd3ca1288801f67957fe2698d2132
ivanbrennan <ivan.brennan@gmail.com>
2015-02-22 15:17:55 -0500

    closer compliance with C styleguide (set noet)

--- ex1.c
+++ ex1.c
@@ -2,13 +2,13 @@
 
 int main(int argc, char *argv[])
 {
-    puts("Hello world.");
-    puts("I'm just starting to learn C.");
-    puts("It was supposed to be harder than this.");
-    puts("Let's see what the first element of argv[] is:");
-    puts(argv[0]);
-    puts("Now let's see what the next element is:");
-    puts(argv[1]);
-    puts("See what happened when we fed `puts' a NULL argument?");
-    return 0;
+	puts("Hello world.");
+	puts("I'm just starting to learn C.");
+	puts("It was supposed to be harder than this.");
+	puts("Let's see what the first element of argv[] is:");
+	puts(argv[0]);
+	puts("Now let's see what the next element is:");
+	puts(argv[1]);
+	puts("See what happened when we fed `puts' a NULL argument?");
+	return 0;
 }
